2309b8cf1950c66e3286dcf5550058f5d3b1e9ba
amgeekz <79773348+amgeekz@users.noreply.github.com>
2025-09-23 19:14:29 +0700

    Add files via upload

--- api/server/webhook/payment.js
+++ api/server/webhook/payment.js
@@ -1,1 +1,6 @@
-
+// api/server/webhook/payment.js
+const app = require('../../../../server');
+module.exports = (req, res) => {
+  req.url = '/webhook/payment' + (req.url.includes('?') ? '?' + req.url.split('?')[1] : '');
+  return app(req, res);
+};
