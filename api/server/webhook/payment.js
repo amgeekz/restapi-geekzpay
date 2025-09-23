3324e451aec2037a5f9c4ecb85e8aa179e6d0506
amgeekz <79773348+amgeekz@users.noreply.github.com>
2025-09-23 20:14:19 +0700

    Delete api/server/webhook/payment.js

--- api/server/webhook/payment.js
+++
@@ -1,6 +0,0 @@
-// api/server/webhook/payment.js
-const app = require('../../../server');
-module.exports = (req, res) => {
-  req.url = '/webhook/payment' + (req.url.includes('?') ? '?' + req.url.split('?')[1] : '');
-  return app(req, res);
-};
