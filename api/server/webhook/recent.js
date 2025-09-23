3c21efe8157b1b3323e8a00b13c2707e4228d62a
amgeekz <79773348+amgeekz@users.noreply.github.com>
2025-09-24 00:26:42 +0700

    Add files via upload

---
+++ api/server/webhook/recent.js
@@ -0,0 +1,6 @@
+const app = require('../../../server');
+
+module.exports = (req, res) => {
+  req.url = '/webhook/recent' + (req.url.includes('?') ? '?' + req.url.split('?')[1] : '');
+  return app(req, res);
+};
