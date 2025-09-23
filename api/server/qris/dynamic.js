a41d7962bc4c37de2e3d113d5d8275d18f4e473a
amgeekz <79773348+amgeekz@users.noreply.github.com>
2025-09-24 00:25:26 +0700

    Add files via upload

--- api/server/qris/dynamic.js
+++ api/server/qris/dynamic.js
@@ -1,1 +1,6 @@
-
+// api/server/qris/dynamic.js
+const app = require('../../../server');
+module.exports = (req, res) => {
+  req.url = '/qris/dynamic' + (req.url.includes('?') ? '?' + req.url.split('?')[1] : '');
+  return app(req, res);
+};
