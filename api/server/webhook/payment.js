6efa2973d43e02c8a41ae1966c4e0936329780d6
amgeekz <79773348+amgeekz@users.noreply.github.com>
2025-09-23 19:30:21 +0700

    Update payment.js

--- api/server/webhook/payment.js
+++ api/server/webhook/payment.js
@@ -1,5 +1,5 @@
 // api/server/webhook/payment.js
-const app = require('../../../../server');
+const app = require('../../../server');
 module.exports = (req, res) => {
   req.url = '/webhook/payment' + (req.url.includes('?') ? '?' + req.url.split('?')[1] : '');
   return app(req, res);
